a447442fc0f42d37e1ccc0fb5830fc0c503b13f8
codemedic <codemedic2@gmail.com>
2026-01-19 09:35:05 +0300

    import err fix

--- polls-backend/src/app/(frontend)/page.tsx
+++ polls-backend/src/app/(frontend)/page.tsx
@@ -1,19 +1,9 @@
-import { headers as getHeaders } from 'next/headers.js'
 import Image from 'next/image'
-import { getPayload } from 'payload'
-import React from 'react'
-import { fileURLToPath } from 'url'
-
 import config from '@/payload.config'
 import './styles.css'
 
 export default async function HomePage() {
-  const headers = await getHeaders()
   const payloadConfig = await config
-  const payload = await getPayload({ config: payloadConfig })
-  const { user } = await payload.auth({ headers })
-
-  const fileURL = `vscode://file/${fileURLToPath(import.meta.url)}`
 
   return (
     <div className="home">
@@ -27,8 +17,6 @@ export default async function HomePage() {
             width={65}
           />
         </picture>
-        {!user && <h1>Welcome to your new project.</h1>}
-        {user && <h1>Welcome back, {user.email}</h1>}
         <div className="links">
           <a
             className="admin"
@@ -47,12 +35,6 @@ export default async function HomePage() {
             Documentation
           </a>
         </div>
-      </div>
-      <div className="footer">
-        <p>Update this page by editing</p>
-        <a className="codeLink" href={fileURL}>
-          <code>app/(frontend)/page.tsx</code>
-        </a>
       </div>
     </div>
   )
