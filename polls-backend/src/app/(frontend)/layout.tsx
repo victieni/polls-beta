95b4d93884347ae42f63dd7ac82a565c07612d72
codemedic <codemedic2@gmail.com>
2026-01-19 09:20:14 +0300

    clerk intergration fix 4

--- polls-backend/src/app/(frontend)/layout.tsx
+++ polls-backend/src/app/(frontend)/layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
+import AuthProvider from '@/providers/AuthProvider'
 import './styles.css'
-import AuthProvider from 'providers/AuthProvider'
 
 export const metadata = {
   description: 'A blank template using Payload in a Next.js app.',
