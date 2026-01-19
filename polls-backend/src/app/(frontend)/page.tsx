77f3bc6ee82ad30bd0b600d11ae73cf4d82f96f7
codemedic <codemedic2@gmail.com>
2026-01-19 12:12:30 +0300

    Added login btn on home Page

--- polls-backend/src/app/(frontend)/page.tsx
+++ polls-backend/src/app/(frontend)/page.tsx
@@ -1,13 +1,14 @@
 import Image from 'next/image'
 import config from '@/payload.config'
 import './styles.css'
+import { SignedIn, SignedOut, SignIn, SignInButton } from '@clerk/nextjs'
 
 export default async function HomePage() {
   const payloadConfig = await config
 
   return (
     <div className="home">
-      <div className="content">
+      <div className="content space-y-4">
         <picture>
           <source srcSet="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg" />
           <Image
@@ -17,15 +18,26 @@ export default async function HomePage() {
             width={65}
           />
         </picture>
+        <p className="text-lg text-green-600 text-center">Polls App</p>
         <div className="links">
-          <a
-            className="admin"
-            href={payloadConfig.routes.admin}
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Go to admin panel
-          </a>
+          <SignedOut>
+            <SignInButton mode="modal">
+              <button className="py-2 px-4 font-medium text-center rounded-lg text-green-600">
+                Sign in
+              </button>
+            </SignInButton>
+          </SignedOut>
+
+          <SignedIn>
+            <a
+              className="admin"
+              href={payloadConfig.routes.admin}
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Go to admin panel
+            </a>
+          </SignedIn>
           <a
             className="docs"
             href="https://payloadcms.com/docs"
